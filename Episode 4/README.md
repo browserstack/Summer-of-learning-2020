92594e6135d2c873d2397e8028205c17efa81688
AutomatedTester <david.burns@theautomatedtester.co.uk>
2020-05-22 14:58:56 +0100

    Summer of Learning Episode 4

--- Episode 4/README.md
+++ Episode 4/README.md
@@ -1,5 +1,18 @@
-# Selenium + BrowserStack at scale
+# Speed up your releases with parallelization in Selenium
 
-David Burns, a core contributor to Selenium, will explain how to plan parallelization more effectively to achieve faster build times, the best ways to maintain test hygiene while scaling your team or automation suite, and how to monitor test feedback effectively.
+This repository contains all the code to run from the Webinar held by [David Burns](https://github.com/automatedtester).
 
-Speaker: [David Burns](https://github.com/automatedtester)
+## Running the demos.
+
+* You will need to check out the [demo site](https://github.com/AutomatedTester/demosite).
+* Install the python requirements with `pip install -r requirements.txt`
+* Run the tests
+``` pytest path/to_tests.py```
+
+* With some of the tests you can change the driver by simply changing the `--driver browser` argument. For running against Firefox you would do ```pytest parallel/site_tests.py --driver Firefox```.
+
+* You can also use [BrowserStack](https://www.browserstack.com) if you set the following environment variables
+```bash
+export BSUSERNAME=browserstack_user
+export BSPASSWORD=browserstack_key
+```
---
+++ Episode 4/parallel/run.sh
@@ -0,0 +1,18 @@
+pytest -n 5  parallel/remote_site_tests.py --driver Chrome --os-version 10 &  PIDCHROME10=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Chrome --os-version 8.1 &  PIDCHROME8_1=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Chrome --os-version 8.1 &  PIDCHROME8=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Firefox --os-version 10 &  PIDFIREFOX10=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Firefox --os-version 8.1 &  PIDFIREFOX8_1=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Firefox --os-version 8 &  PIDFIREFOX8=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Edge --os-version 10 &  PIDEDGE10=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Edge --os-version 8.1 &  PIDEDGE8_1=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Edge --os-version 8 &  PIDEDGE8=$!
+pytest -n 5  parallel/remote_site_tests.py --driver IE  --os-version 10 &  PIDIE10=$!
+pytest -n 5  parallel/remote_site_tests.py --driver IE  --os-version 8.1 &  PIDIE81=$!
+pytest -n 5  parallel/remote_site_tests.py --driver IE  --os-version 8 &  PIDIE8=$!
+pytest -n 5  parallel/remote_site_tests.py --driver IE  --os-version 7 &  PIDIE7=$!
+pytest -n 5  parallel/remote_site_tests.py --driver Safari &  PIDSAFARI=$!
+pytest -n 5  parallel/remote_site_tests.py --driver 'Galaxy S20' &  PIDGALAXY=$!
+pytest -n 5  parallel/remote_site_tests.py --driver ios &  PIDSAFARI=$!
+
+wait
